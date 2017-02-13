341ecb92209e46f2b7af58b8ca27b0fff830919f
Alex Boroda <boroda@amazon.com>
2017-02-13 10:55:43 -0800

    more input

--- cards.py
+++ cards.py
@@ -6,13 +6,12 @@ colors = ['red', 'black']
 values=['1','2','3','4','5','6','7','8','9','10','jack', 'queen','king','ace']
 picked = []
 
-
 deck = []
 your_cards = []
 
 for card in range(0,51):
         for suit in suits:
-                for color in colors:
+                 for color in colors:
                         for value in values:
                                 deck.append({'color' : '%s' % color, 
                                       'suit' : '%s' % suit,
@@ -29,10 +28,10 @@ your_value = raw_input("High or low? ")
 your_cards.append(deck[randint(0,52)])
 
 print your_cards
-
-if your_value == 'high':
-        if your_cards[1]['value'] > your_cards[0]['value']:
-                print "give a drink"
+# TODO: compare face cards
+if (your_cards[1]['value'] > your_cards[0]['value']) and your_value == 'high' :
+        print "give a drink"
+elif (your_cards[1]['value'] < your_cards[0]['value']) and your_value =='low':
+        print "give a drink"
 else:
-         if your_cards[1]['value'] < your_cards[0]['value']:
-                print "give a drink"
+        print "take a drink"
